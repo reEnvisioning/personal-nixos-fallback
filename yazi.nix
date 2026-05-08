8aebd463e692f37e2b8e181223af099830a238a8
reEnvisioning <reenvision@proton.me>
2026-05-08 15:58:56 +0200

    Add files via upload

--- yazi.nix
+++ yazi.nix
@@ -1,25 +1,29 @@
 { pkgs, ... }:
 let
   theme = import ./theme.nix;
-  activeFlavor = if theme.mode == "dark" then "oled-dark" else "oled-light";
 in {
   programs.yazi = {
     enable = true;
     flavors = {
-      "oled-dark" = ./yazi-themes/dark.toml;
-      "oled-light" = ./yazi-themes/light.toml;
+      "dark"  = ./yazi-themes/dark.toml;
+      "light" = ./yazi-themes/light.toml;
     };
-    settings.yazi = {
-      flavor.use = activeFlavor;
-      manager.show_hidden = true;
-      sort_dir_first = true;
-      sort_by = "natural";
-    };
-    settings.opener = {
-      text  = [{ run = ''nvim "$@"''; block = true; }];
-      image = [{ run = ''feh "$@"''; }];
-      video = [{ run = ''mpv --loop-file "$@"''; }];
-      audio = [{ run = ''mpv --loop-file "$@"''; }];
+    settings = {
+      yazi = {
+        flavor.use = theme.mode;
+        manager = {
+          show_hidden = true;
+          sort_dir_first = true;
+          sort_by = "natural";
+        };
+        opener = {
+          text  = [{ run = ''nvim "$@"''; block = true; }];
+          image = [{ run = ''feh "$@"''; }];
+          video = [{ run = ''mpv --loop-file "$@"''; }];
+          audio = [{ run = ''mpv --loop-file "$@"''; }];
+        };
+      };
+      theme = { };
     };
   };
 }
