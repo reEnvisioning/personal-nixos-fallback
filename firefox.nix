35eae8a99c6d01fdb57d9fb91af4e0c70fc528ff
reEnvisioning <reenvision@proton.me>
2026-05-09 21:53:46 +0200

    Add files via upload

--- firefox.nix
+++ firefox.nix
@@ -1,11 +1,13 @@
-{ ... }: {
+{ pkgs, ... }:
+let theme = import ./theme.nix;
+in {
   programs.firefox = {
     enable = true;
     profiles.default = {
       settings = {
-        "ui.systemUsesDarkTheme" = 3;
-        "browser.theme.toolbar-theme" = 2;
-        "browser.theme.content-theme" = 2;
+        "ui.systemUsesDarkTheme" = if theme.mode == "dark" then 1 else 0;
+        "browser.theme.toolbar-theme" = if theme.mode == "dark" then 1 else 0;
+        "browser.theme.content-theme" = if theme.mode == "dark" then 1 else 0;
       };
     };
   };
