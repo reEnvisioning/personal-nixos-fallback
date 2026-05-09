9fe9de6e61296e6683de7ef584cede5955707a66
reEnvisioning <reenvision@proton.me>
2026-05-09 22:51:06 +0200

    Add files via upload

--- yazi.nix
+++ yazi.nix
@@ -9,9 +9,17 @@
         sort_by = "natural";
       };
       opener = {
-        edit = [{ run = "nvim %s"; block = true; }];
-        open = [{ run = "feh %s"; }];
-        play = [{ run = "mpv --loop-file %s"; orphan = true; }];
+        edit = [{ run = ''nvim "$@"''; block = true; for = "unix"; }];
+        open = [{ run = ''feh "$@"''; for = "unix"; }];
+        play = [{ run = ''mpv --loop-file "$@"''; orphan = true; for = "unix"; }];
+      };
+      open = {
+        rules = [
+          { mime = "text/*"; use = [ "edit" "open" ]; }
+          { mime = "image/*"; use = [ "open" "edit" ]; }
+          { mime = "{audio,video}/*"; use = [ "play" "open" ]; }
+          { url = "*"; use = [ "open" "edit" ]; }
+        ];
       };
     };
   };
