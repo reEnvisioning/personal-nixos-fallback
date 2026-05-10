23e401d33816d5379d21ecc57b30b68ec6cab51a
reEnvisioning <reenvision@proton.me>
2026-05-10 20:48:05 +0200

    Add files via upload

--- home.nix
+++ home.nix
@@ -62,6 +62,7 @@ in {
   };
 
   home.file.".mozilla/firefox/profiles.ini".force = true;
+  home.file.".mozilla/firefox/default/user.js".force = true;
 
   home.activation.applyDefaultTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     switch-theme "${theme.default}" 2>/dev/null || true
