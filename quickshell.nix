b173ff4878f4e23ceb69a845ebed9c818c793219
reEnvisioning <reenvision@proton.me>
2026-05-16 14:58:07 +0200

    Delete quickshell.nix

--- quickshell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-let
-  shellQml = builtins.readFile ./quickshell-config/shell.qml;
-in {
-  home.packages = with pkgs; [ quickshell ];
-
-  xdg.configFile."quickshell/shell.qml".text = shellQml;
-}
