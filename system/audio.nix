f5511d44af7912da8bbcd30c3f6badffa12fbd7d
reEnvisioning <reenvision@proton.me>
2026-05-21 20:42:11 +0200

    Add files via upload

--- system/audio.nix
+++ system/audio.nix
@@ -6,12 +6,5 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
-
-    extraConfig = {
-      "context.properties" = {
-        "default.clock.allowed-rates" = [ 48000 ];
-      };
-    };
   };
 }
