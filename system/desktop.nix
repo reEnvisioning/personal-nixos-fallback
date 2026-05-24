b8cc55f7b78b5e3398453d1a4f41ace788c0d111
reEnvisioning <reenvision@proton.me>
2026-05-24 12:24:59 +0200

    Add files via upload

--- system/desktop.nix
+++ system/desktop.nix
@@ -41,8 +41,6 @@
     "nvidia-drm.modeset=1"
   ];
 
-  boot.initrd.luks.devices."luks-26fcc57f-6de2-4ea0-9c1e-9411b537d0ae".device = "/dev/disk/by-uuid/26fcc57f-6de2-4ea0-9c1e-9411b537d0ae";
-
   time.timeZone = "Europe/Berlin";
   console.keyMap = "us";
 
