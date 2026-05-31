be76018889ca3f2963d42872c82c6a9934eb94bf
reEnvisioning <reenvision@proton.me>
2026-05-31 21:57:39 +0200

    Add files via upload

--- system/polkit.nix
+++ system/polkit.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }: {
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
-      if (action.id.indexOf("org.freedesktop.udisks2.") === 0 &&
+      if ((action.id == "org.freedesktop.udisks2.filesystem-mount" ||
+           action.id == "org.freedesktop.udisks2.filesystem-unmount" ||
+           action.id == "org.freedesktop.udisks2.eject-media") &&
           subject.isInGroup("disk")) {
-        return polkit.Result.YES;
+        try {
+          var drive = action.lookup("drive");
+          if (drive && drive.removable) {
+            return polkit.Result.YES;
+          }
+        } catch (e) {}
       }
     });
   '';
