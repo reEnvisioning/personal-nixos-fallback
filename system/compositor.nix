8211febd3e1975b3dae576c0b65274c1faf84d45
reEnvisioning <reenvision@proton.me>
2026-06-05 16:30:14 +0200

    Add files via upload

--- system/compositor.nix
+++ system/compositor.nix
@@ -3,6 +3,15 @@
     enable = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gnome xdg-desktop-portal-gtk ];
+    config.common = {
+      default = [ "gnome" "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     xwayland-satellite
     nautilus
