89c3c6462cd23995b2a756f269cc7abce0f82a07
reEnvisioning <reenvision@proton.me>
2026-06-05 19:15:01 +0200

    Add files via upload

--- home/visionary/ssh.nix
+++ home/visionary/ssh.nix
@@ -5,43 +5,45 @@
     enable = true;
     enableDefaultConfig = false;
 
-    settings = {
+    matchBlocks = {
       "*" = {
-        ForwardAgent = false;
-        Compression = true;
-        HashKnownHosts = true;
-        ServerAliveInterval = 30;
-        ServerAliveCountMax = 3;
+        forwardAgent = false;
+        compression = true;
+        hashKnownHosts = true;
+        serverAliveInterval = 30;
+        serverAliveCountMax = 3;
 
-        ControlMaster = "auto";
-        ControlPath = "~/.ssh/control/%r@%h:%p";
-        ControlPersist = "10m";
+        controlMaster = "auto";
+        controlPath = "~/.ssh/control/%r@%h:%p";
+        controlPersist = "10m";
 
-        KexAlgorithms = "sntrup761x25519-sha512@openssh.com,diffie-hellman-group-exchange-sha256";
-        Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
-        MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
-        HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519";
-        PubkeyAcceptedAlgorithms = "ssh-ed25519";
+        extraOptions = {
+          KexAlgorithms = "sntrup761x25519-sha512@openssh.com,diffie-hellman-group-exchange-sha256";
+          Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
+          MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
+          HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519";
+          PubkeyAcceptedAlgorithms = "ssh-ed25519";
 
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        ChallengeResponseAuthentication = false;
-        GSSAPIAuthentication = false;
-        PreferredAuthentications = "publickey";
+          PasswordAuthentication = "no";
+          KbdInteractiveAuthentication = "no";
+          ChallengeResponseAuthentication = "no";
+          GSSAPIAuthentication = "no";
+          PreferredAuthentications = "publickey";
 
-        ForwardX11 = false;
-        ForwardX11Trusted = false;
-        AllowTcpForwarding = false;
-        AllowStreamLocalForwarding = false;
-        Tunnel = false;
+          ForwardX11 = "no";
+          ForwardX11Trusted = "no";
+          AllowTcpForwarding = "no";
+          AllowStreamLocalForwarding = "no";
+          Tunnel = "no";
 
-        RekeyLimit = "1G 1h";
-        StrictHostKeyChecking = "ask";
-        UpdateHostKeys = "ask";
+          RekeyLimit = "1G 1h";
+          StrictHostKeyChecking = "ask";
+          UpdateHostKeys = "ask";
 
-        UserKnownHostsFile = "~/.ssh/known_hosts";
+          UserKnownHostsFile = "~/.ssh/known_hosts";
 
-        LogLevel = "VERBOSE";
+          LogLevel = "VERBOSE";
+        };
       };
     };
   };
