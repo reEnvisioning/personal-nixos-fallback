f919630c8f8a66d4ec59edf0e5c26c29ecc36850
reEnvisioning <reenvision@proton.me>
2026-06-05 17:27:25 +0200

    Add files via upload

--- system/compositor.nix
+++ system/compositor.nix
@@ -1,20 +1,25 @@
 { pkgs, ... }: {
   programs.niri = {
     enable = true;
-    useNautilus = false;
+    useNautilus = true;
   };
 
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gnome xdg-desktop-portal-gtk ];
-    config.common = {
-      default = [ "gnome" "gtk" ];
-      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-    };
+    config.niri."org.freedesktop.impl.portal.FileChooser" = "nautilus";
   };
 
   environment.systemPackages = with pkgs; [
     xwayland-satellite
     nautilus
+    (runCommandLocal "nautilus-portal" { } ''
+      mkdir -p $out/share/xdg-desktop-portal/portals
+      cat > $out/share/xdg-desktop-portal/portals/nautilus.portal <<EOF
+      [portal]
+      DBusName=org.gnome.Nautilus
+      Interfaces=org.freedesktop.impl.portal.FileChooser
+      EOF
+    '')
   ];
 }
