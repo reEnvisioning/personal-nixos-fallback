dd37d8b205a81e492b48d3cf931e3c48c04cfaad
reEnvisioning <reenvision@proton.me>
2026-06-21 21:25:56 +0200

    Delete system/virtualisation.nix

--- system/virtualisation.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, config, ... }: {
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableKvm = true;
-  virtualisation.virtualbox.host.addNetworkInterface = false;
-  boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ virtualbox ];
-}
