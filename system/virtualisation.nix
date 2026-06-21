ede98cc7e4af05fe8c24517ab23b716149f33c20
reEnvisioning <reenvision@proton.me>
2026-06-21 19:42:56 +0200

    Add files via upload

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableKvm = true;
   virtualisation.virtualbox.host.addNetworkInterface = false;
+  boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ virtualbox ];
 }
