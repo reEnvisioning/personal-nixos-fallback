8c2664a1e1ec5db9fcd4684848b7e76854c969f5
reEnvisioning <reenvision@proton.me>
2026-06-21 18:13:21 +0200

    Add files via upload

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableKvm = true;
+  virtualisation.virtualbox.host.addNetworkInterface = false;
 }
