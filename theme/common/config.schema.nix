94379ee5b174a02373fc80a99a364abf6b25c248
reEnvisioning <282255751+reEnvisioning@users.noreply.github.com>
2026-06-26 18:02:28 +0200

    Delete theme directory

--- theme/common/config.schema.nix
+++
@@ -1,8 +0,0 @@
-{ theme, uiScale ? 1, perMonitor ? {} }:
-builtins.toJSON ({
-  uiScale = uiScale;
-  themeName = theme.name or "";
-  mode = theme.mode or "dark";
-  colors = theme.colors or {};
-  monitors = perMonitor;
-})
--- theme/quickshell/lib/CAnim.qml
+++
@@ -1,7 +0,0 @@
-import QtQuick
-
-ColorAnimation {
-    duration: 200
-    easing.type: Easing.Bezier
-    easing.bezierCurve: [0.34, 0.8, 0.34, 1.0]
-}
