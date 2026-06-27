2acdad6a01ebaf937a7b827524d690eedf0503a1
reEnvisioning <282255751+reEnvisioning@users.noreply.github.com>
2026-06-27 21:01:59 +0200

    Delete secret.nix

--- secret.nix
+++
@@ -1,8 +0,0 @@
-{
-  serverIp    = "<server-public-ip>";
-  serverPort  = 443;
-  serverPub   = "<server-public-key>";
-  tunnelIp    = "10.88.0.2/32";
-  serverTunIp = "10.88.0.1";
-  gateway     = "<lan-gateway>";
-}
