3346b182bb50d2295e4af242711eaeefb0467c63
reEnvisioning <282255751+reEnvisioning@users.noreply.github.com>
2026-08-09 14:31:57 +0000

    Use zsh hook for output spacing

--- home/visionary/home.nix
+++ home/visionary/home.nix
@@ -25,7 +25,9 @@
     enable = true;
     enableCompletion = true;
     initContent = ''
-      preexec() { print }
+      autoload -Uz add-zsh-hook
+      add-zsh-hook preexec '_re_prompt_spacing_preexec'
+      _re_prompt_spacing_preexec() { print }
     '';
   };
 
