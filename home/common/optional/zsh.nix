66c24e7f7af6cd70a3ea0c383f9d6bd6e6985267
reEnvisioning <282255751+reEnvisioning@users.noreply.github.com>
2026-08-13 17:25:50 +0000

    Fix Zsh input bindings

--- home/common/optional/zsh.nix
+++ home/common/optional/zsh.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
-  home.sessionVariables.NIX_BUILD_SHELL = "${pkgs.zsh}/bin/zsh";
   programs.zsh = {
+    shellAliases.nix-shell = "nix-shell --command ${pkgs.zsh}/bin/zsh";
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
@@ -13,13 +13,10 @@
       add-zsh-hook preexec '_re_prompt_spacing_preexec'
       _re_prompt_spacing_preexec() { print }
 
-      _re_up_line_or_beginning_search() { fc -RI; zle history-beginning-search-backward }
-      _re_down_line_or_beginning_search() { fc -RI; zle history-beginning-search-forward }
-      zle -N _re_up_line_or_beginning_search
-      zle -N _re_down_line_or_beginning_search
       zmodload zsh/terminfo
-      [[ -n ''${terminfo[kcuu1]} ]] && bindkey "''${terminfo[kcuu1]}" _re_up_line_or_beginning_search
-      [[ -n ''${terminfo[kcud1]} ]] && bindkey "''${terminfo[kcud1]}" _re_down_line_or_beginning_search
+      [[ -n ''${terminfo[kcuu1]} ]] && bindkey "''${terminfo[kcuu1]}" history-beginning-search-backward
+      [[ -n ''${terminfo[kcud1]} ]] && bindkey "''${terminfo[kcud1]}" history-beginning-search-forward
+      bindkey '^I' autosuggest-accept
     '';
   };
 }
